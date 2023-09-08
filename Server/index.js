7ba7f1b551b7a44c59f7a682a2fee4218b9a749b
Stephan Granzow <stephangranzow@MBP-von-Stephan.fritz.box>
2023-09-08 13:43:29 +0200

    connect db

--- Server/index.js
+++ Server/index.js
@@ -1,30 +1,37 @@
-
-
 // IMPORT from Packages
-const express =require('express');
+const express = require('express');
+const { env_db } = require('../.env.js');
+  // Ensure the path './env' points correctly to the env.js file
 
-// Import from other fiels
+const mongoose = require("mongoose");
+require('dotenv').config();  // Load environment variables from .env file
 
-const authRouter = require("./routes/auth")
+// Import from other files
+const authRouter = require("./routes/auth");
+
 // INIT
-const Port=3000;
+const Port = 3000;
+const DB = env_db;
+ // Assuming you named it DB_STRING in your .env file
+const app = express();
 
-const app= express();
 // middleware
+app.use(express.json())
 app.use(authRouter);
+
 // CLIENT -> SERVER-> CLIENT
-
-
-
-
-
 
 // Create an API
 // GET, PUT, POST, DELETE, UPDATE -> CRUD 
-//http://youripdaddress/ 
+//http://youripdaddress/
 
+// connection
+mongoose.connect(DB).then(() => {
+    console.log("Database mongoose connection successful");
+}).catch(e => {
+    console.log(e);
+});
 
-
-app.listen(Port, function(){
-console.log("port is connected! juhuuuuuu")
-})
+app.listen(Port, function() {
+    console.log("port is connected! juhuuuuuu");
+});
--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,8 +1,31 @@
 const express =require('express');
+const User = require('../models/user');
 
 const authRouter =express.Router();
 
-authRouter.get("/user", (req, res)=>{
-    res.json({user: "Stephan King"})
+
+authRouter.post("/api/signup", async function(req, res){
+    try {
+        const {name, email, password}  =  req.body;
+
+ const existingUser= await  User.findOne({
+    email
+ });
+
+ if(existingUser){
+    return res.status(400).json( {msg:"Password oder Email ist falsch, bzw. existiert schon"})
+ }
+
+ let user=new User({
+    email, password, name,
+ })
+ user=await user.save();
+ res.json(user);
+    } catch (error) {
+        res.status(500).json({error:e.message})
+    }
+ 
+
 })
+
 module.exports=authRouter;
