cf9fd4e9a305f7a6a8a798030dc0eb583b93b43a
Stephan Granzow <stephangranzow@MBP-von-Stephan.fritz.box>
2023-09-11 16:17:24 +0200

    createAccount

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,30 +1,43 @@
-
-
+import 'package:amazon_clone/constants/error_handling.dart';
 import 'package:amazon_clone/constants/global_veriables.dart';
+import 'package:amazon_clone/constants/utilis.dart';
 import 'package:amazon_clone/models/user.dart';
+import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-class AuthService{
-  
-void createAccount({  required String email,   required String name,   required String password, })async{
 
-try{
-  User user=User(id: "", name: name, password: password, address: "", type: "", token: "", email: email);
- 
- http.Response res= await http.post(Uri.parse("$uri/api/signup"), body:
-    user.toJson(),headers: <String, String>{
-      'Content-Type':'application/json; charset=UTF-8'
+class AuthService {
+  void createAccount({
+    required String email,
+    required BuildContext context,
+    required String name,
+    required String password,
+  }) async {
+    try {
+      User user = User(
+          id: "",
+          name: name,
+          password: password,
+          address: "",
+          type: "",
+          token: "",
+          email: email);
+
+      http.Response res = await http.post(Uri.parse("$uri/api/signup"),
+          body: user.toJson(),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8'
+          });
+
+      // ignore: use_build_context_synchronously
+      httpErrorHandler(
+          response: res,
+          context: context,
+          onSuccess: () {
+            showSnackBar(context, "Account erfolgreich erstellt");
+          });
+    } catch (e) {
+      showSnackBar(context, e.toString());
     }
-    
-
-  );
-}catch(e){
-
-}
-
-}
-
-
-
-
+  }
 }
