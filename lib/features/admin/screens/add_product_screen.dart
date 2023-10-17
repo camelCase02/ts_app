2486f6aebf06cb76b5c8613f2e63ad03ad146cfc
Stephan Granzow <stephan_granzow@web.de>
2023-10-17 13:03:17 +0200

    add products screen

--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -1,6 +1,6 @@
-
 import 'package:amazon_clone/constants/global_veriables.dart';
 import 'package:flutter/material.dart';
+import 'package:dotted_border/dotted_border.dart';
 
 class AddProductScreen extends StatefulWidget {
   static const String routeName = "/add-product";
@@ -13,47 +13,37 @@ class AddProductScreen extends StatefulWidget {
 class _AddProductScreenState extends State<AddProductScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-     appBar: PreferredSize(
+    return Scaffold(
+      appBar: PreferredSize(
           preferredSize: const Size.fromHeight(50),
           child: AppBar(
-            flexibleSpace: Container(
-              decoration:
-                  const BoxDecoration(gradient: GlobalVariables.appBarGradient),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                    gradient: GlobalVariables.appBarGradient),
+              ),
+              title: const Text(
+                "Neues Produkt hinzufügen",
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(color: Colors.black),
+              ))),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Form(
+            child: Column(
               children: [
-                Container(
-                  alignment: Alignment.topLeft,
-                  child: Image.asset(
-                    "assets/amazon_in.png",
-                    width: 120,
-                    height: 45,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.only(
-                    left: 15,
-                    right: 15,
-                  ),
-                  child: const Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(
-                          left: 15,
-                          right: 15,
-                        ),
-                        child: Icon(Icons.notifications_outlined),
-                      ),
-                      Icon(Icons.search),
-                    ],
-                  ),
-                )
+                DottedBorder(child: Container(width: double.infinity,height: 150, decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+        
+                  child: Column(children: [
+                    const Icon(Icons.folder_open_outlined, size: 40,), 
+                    const SizedBox(height: 15,),
+                    Text("Selekt Produkt Bild", style: TextStyle(color: Colors.grey.shade400),)
+                  ],),))
               ],
             ),
-          )),
-      
-      body: Center(child: Text("Add product screen"),),);
+          ),
+        ),
+      ),
+    );
   }
 }
